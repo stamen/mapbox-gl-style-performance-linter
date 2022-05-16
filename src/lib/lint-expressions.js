a8e76becbae1078ee802069395be54b18c7ebfd3
Parlato <alparlat@3c063012601d.ant.amazon.com>
2022-05-16 14:34:57 -0400

    add linter to primary export

--- src/lib/lint-expressions.js
+++ src/lib/lint-expressions.js
@@ -3,6 +3,7 @@ import { lintSingleMatchExpressions } from './expressions/single-match';
 const lintExpressions = style => {
   let warnings = [];
   warnings = warnings.concat(lintSingleMatchExpressions(style));
+  warnings = warnings.concat(lintExpressionDuplicateOutputs(style));
   return warnings;
 };
 
